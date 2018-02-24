8454b9e97dca9ccb14896abe1a3f23cc53fc45cd
taemon1337 <taemon1337@gmail.com>
2018-02-24 15:35:41 -0500

    moved to ubuntu:latest

--- chrome.sh
+++ chrome.sh
@@ -6,7 +6,6 @@ if [ "$1" == "--clean" ]; then
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /dev/snd:/dev/snd \
   -v /dev/shm:/dev/shm \
-  --privileged \
   -e uid=$(id -u) \
   -e gid=$(id -g) \
   -e DISPLAY=unix$DISPLAY \
@@ -19,7 +18,6 @@ else
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /dev/snd:/dev/snd \
   -v /dev/shm:/dev/shm \
-  --privileged \
   -e uid=$(id -u) \
   -e gid=$(id -g) \
   -e DISPLAY=unix$DISPLAY \
